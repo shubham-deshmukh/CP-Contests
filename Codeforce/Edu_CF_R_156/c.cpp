82c383c52832769a944f4ebf5ab73ae318388a88
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2023-10-21 15:21:20 +0530

    Practise CF Edu R 156 : C

---
+++ Codeforce/Edu_CF_R_156/c.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define lld long double
+#define ull unsigned long long
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define rep(i,start,end) for(i = start; start < end ? i < end : i > end; start < end ? i += 1 : i -= 1)
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline ll gcd(ll a, ll b) {ll r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+
+
+bool cmp(pair<ull,ull> x, pair<ull,ull> y) {
+    if(x.ss != y.ss) return (x.ss > y.ss); // descresing order of 2nd elt
+    else return (x.ff < y.ff); // incresing order of 1st elt
+}
+
+void solve() {
+    string s;
+    long long pos;
+    cin >> s >> pos;
+
+    int n = (int)s.size();
+    pos--;
+    // find the number of removal and position of character
+    // after removal
+    int rem, new_pos;
+    for(int i = 0; i < n; i++) {
+        int len = n-i;
+        if(pos < len) {
+            rem = i;
+            new_pos = pos;
+            break;
+        }
+        pos -= len;
+    }
+
+    // perform the removal 
+    string st;
+    for(auto &ch: s) {
+        while(rem > 0 && !st.empty() && ch < st.back()) {
+            st.pop_back();
+            rem--;
+        }
+        st += ch;
+    }
+    cout << st[new_pos];
+}
+int main() {
+    // your code goes here
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+        freopen("../../../error.txt", "w", stderr);
+    #endif
+    
+    ll t = 1;
+    cin >> t;
+    
+    while(t--) {
+        solve();
+        
+        // if(t > 0) cout << endl;
+    }
+    cout<<fixed<<setprecision(10);
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
