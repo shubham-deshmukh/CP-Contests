ff8cd193cda4ed8ee7a4e9bea3f489f03f4807cf
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2023-12-12 17:20:04 +0530

    Practise CF Edu R 155 : A,B

---
+++ Codeforce/Edu_CF_R_155/a.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define lld long double
+#define ull unsigned long long
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define rep(i,start,end) for(i = start; start < end ? i < end : i > end; start < end ? i += 1 : i -= 1)
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline ll gcd(ll a, ll b) {ll r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+
+
+bool cmp(pair<ull,ull> x, pair<ull,ull> y) {
+    if(x.ss != y.ss) return (x.ss > y.ss); // descresing order of 2nd elt
+    else return (x.ff < y.ff); // incresing order of 1st elt
+}
+
+struct Player {
+    int s, e;
+
+    bool operator<(const Player &y) { return s < y.s; }
+};
+
+void solve() {
+    int N;
+    cin >> N;
+
+    vector<Player> p(N);
+
+    for(int i = 0; i < N; i++)
+        cin >> p[i].s >> p[i].e;
+    
+    Player mono = p[0];
+
+    sort(p.begin(), p.end());
+
+    int pos = lower_bound(p.begin(), p.end(), mono) - p.begin();
+
+    for(int i = pos+1; i < N; i++) {
+        if(p[i].e >= mono.e) {
+            cout << -1;
+            return;
+        }
+    }
+
+    cout << mono.s;
+
+}
+int main() {
+    // your code goes here
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+        freopen("../../../error.txt", "w", stderr);
+    #endif
+    
+    ll t = 1;
+    cin >> t;
+    
+    while(t--) {
+        solve();
+        
+        if(t > 0) cout << endl;
+    }
+    cout<<fixed<<setprecision(10);
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
---
+++ Codeforce/Edu_CF_R_155/b.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define lld long double
+#define ull unsigned long long
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define rep(i,start,end) for(i = start; start < end ? i < end : i > end; start < end ? i += 1 : i -= 1)
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline ll gcd(ll a, ll b) {ll r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+
+
+bool cmp(pair<ull,ull> x, pair<ull,ull> y) {
+    if(x.ss != y.ss) return (x.ss > y.ss); // descresing order of 2nd elt
+    else return (x.ff < y.ff); // incresing order of 1st elt
+}
+
+void solve() {
+    int N;
+    cin >> N;
+
+    vector<long long> A(N), B(N);
+    for(auto &a: A) cin >> a;
+    for(auto &b: B) cin >> b;
+
+    long long col_sum = 0, row_sum = 0;
+    row_sum = accumulate(A.begin(), A.end(), 0LL);
+    col_sum = accumulate(B.begin(), B.end(), 0LL);
+
+    long long row_min = *min_element(A.begin(), A.end());
+    long long col_min = *min_element(B.begin(),  B.end());
+
+    long long c1 = row_sum + N*col_min;
+    long long c2 = col_sum + N*row_min;
+
+    cout << min(c1, c2);
+    
+}
+int main() {
+    // your code goes here
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+        freopen("../../../error.txt", "w", stderr);
+    #endif
+    
+    ll t = 1;
+    cin >> t;
+    
+    while(t--) {
+        solve();
+        
+        if(t > 0) cout << endl;
+    }
+    cout<<fixed<<setprecision(10);
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
