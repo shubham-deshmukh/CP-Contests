4d7cc69511ba46345b34826c680a63aa51262b6d
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2023-12-15 09:15:15 +0530

    Update CP template

--- Template/Basic/template.cpp
+++ Template/Basic/template.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <bits/stdc++.h>
+
 using namespace std;
 
 #define pb push_back
@@ -14,18 +15,32 @@ using namespace std;
 #define endl "\n"
 #define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
 
 const int MAX_N = 1e5;
 const int mod = 1e9+7;
 
 inline ll gcd(ll a, ll b) {ll r; while (b) {r = a % b; a = b; b = r;} return a;}
 inline ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
-
-
-bool cmp(pair<ull,ull> x, pair<ull,ull> y) {
-    if(x.ss != y.ss) return (x.ss > y.ss); // descresing order of 2nd elt
-    else return (x.ff < y.ff); // incresing order of 1st elt
-}
 
 void solve() {
 
