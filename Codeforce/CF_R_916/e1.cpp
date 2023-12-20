a8afdd6bf535200149bd5f1d0d263eccb4bbb8a3
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2023-12-20 21:59:19 +0530

    Practise CF R 915 (Div 3): A,B,C,D,E1,E2

---
+++ Codeforce/CF_R_916/e1.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define rep(i,start,end) for(i = start; start < end ? i < end : i > end; start < end ? i += 1 : i -= 1)
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
+using pii = pair<int,int>;
+#define int int64_t
+
+inline long long gcd(long long a, long long b) {long long r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline long long lcm(long long a, long long b) {return a / gcd(a, b) * b;}
+
+void solve() {
+    int N;
+    cin >> N;
+
+    vector<int> A(N), B(N);
+    for(auto &a: A) cin >> a;
+    for(auto &b: B) cin >> b;
+
+    vector<pii> diff(N);
+    for(int i = 0; i < N; i++) {
+        diff[i] = {A[i] + B[i], i};
+    }
+
+    sort(diff.begin(), diff.end(), [&](const pii &a, const pii &b) {
+        return a.first > b.first;
+    });
+
+    int score = 0;
+    int turn = 1;
+    for(int i = 0; i < N; i++) {
+        int idx = diff[i].second;
+        if(turn) {
+            score += A[idx] - 1;
+        }
+        else {
+            score -= B[idx] - 1;
+        }
+        turn = 1 - turn;
+    }
+
+    cout << score;
+}
+signed main() {
+    // your code goes here
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+        freopen("../../../error.txt", "w", stderr);
+    #endif
+    
+    long long t = 1;
+    cin >> t;
+    
+    while(t--) {
+        solve();
+        
+        if(t > 0) cout << endl;
+    }
+    cout<<fixed<<setprecision(10);
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
