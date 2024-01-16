07fff5fee2e46be9eb3c898df4998e5ad0956a42
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2024-01-16 21:35:06 +0530

    Practise CF R 920 : A,B,C,D

---
+++ Codeforce/CF_R_920/c.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define rep(i,start,end) for(i = start; start < end ? i < end : i > end; start < end ? i += 1 : i -= 1)
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline long long gcd(long long a, long long b) {long long r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline long long lcm(long long a, long long b) {return a / gcd(a, b) * b;}
+
+#define int int64_t 
+
+void solve() {
+    int n, capacity, rate, b;
+    cin >> n >> capacity >> rate >> b;
+
+    vector<int> A(n);
+    for(auto &a: A) cin >> a;
+
+    int pre = 0;
+
+    for(int i = 0; i < n; i++) {
+        int req = capacity - min((A[i]-pre)*rate, b);
+        if(req <= 0) {
+            cout << "NO";
+            return;
+        }
+
+        capacity = req;
+        pre = A[i];
+    }
+
+    cout << "YES";
+}
+signed main() {
+    // your code goes here
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+        freopen("../../../error.txt", "w", stderr);
+    #endif
+    
+    long long t = 1;
+    cin >> t;
+    
+    while(t--) {
+        solve();
+        
+        if(t > 0) cout << endl;
+    }
+    cout<<fixed<<setprecision(10);
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
---
+++ Codeforce/CF_R_920/d.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define rep(i,start,end) for(i = start; start < end ? i < end : i > end; start < end ? i += 1 : i -= 1)
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline long long gcd(long long a, long long b) {long long r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline long long lcm(long long a, long long b) {return a / gcd(a, b) * b;}
+
+// Debug Template
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+// End of debugging template
+
+#define int int64_t
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> A(n);
+    for(auto &a: A)
+        cin >> a;
+
+    vector<int> B(m);
+    for(auto &b: B)
+        cin >> b;
+    
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    int s = 0, e = m-1;
+    int diff = 0;
+
+    int i = 0, j = n-1;
+    while(i <= j) {
+        
+        int diff1 = abs(A[i]-B[e]);
+        int diff2 = abs(A[j]-B[s]);
+
+        diff += max(diff1, diff2);
+
+        if(diff1 < diff2) {
+            s++;
+            j--;
+        }
+        else {
+            e--;
+            i++;
+        }
+    }
+
+    cout << diff;
+}   
+signed main() {
+    // your code goes here
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+        freopen("../../../error.txt", "w", stderr);
+    #endif
+    
+    long long t = 1;
+    cin >> t;
+    
+    while(t--) {
+        solve();
+        
+        if(t > 0) cout << endl;
+    }
+    cout<<fixed<<setprecision(10);
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
