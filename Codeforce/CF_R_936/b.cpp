6c845351287a5bae2193ed91f970a55706592966
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2024-03-22 22:20:39 +0530

    Live CF R 936 (div 2) : A, B

---
+++ Codeforce/CF_R_936/b.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../../../debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
+
+const int MAX_N = 1e5;
+
+inline long long gcd(long long a, long long b) {long long r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline long long lcm(long long a, long long b) {return a / gcd(a, b) * b;}
+
+#define int int64_t
+const int mod = 1e9+7;
+
+int binpow(int a, int b, int m) {
+    a %= m;
+    int res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> A(n);
+    for(auto &a: A) cin >> a;
+
+    int mx = A[0];
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += A[i];
+        if(sum < 0)
+            sum = 0;
+        mx = max(mx, sum);
+    }
+    int tot = accumulate(A.begin(), A.end(), int(0));
+    tot %= mod;
+    mx %= mod;
+    int extra = mx * (binpow(int(2),k, mod) - int(1));
+    extra %= mod;
+    tot += extra;
+    tot %= mod;
+    cout << (tot % mod + mod)%mod;
+}
+signed main() {
+    // your code goes here
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+        freopen("../../../error.txt", "w", stderr);
+    #endif
+    
+    long long t = 1;
+    cin >> t;
+    
+    while(t--) {
+        solve();
+        
+        if(t > 0) cout << endl;
+    }
+    cout<<fixed<<setprecision(10);
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
