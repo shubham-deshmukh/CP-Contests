65579e6c63909b2d77320f66848d7613c454c2dd
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2024-04-17 08:51:24 +0530

    Practise CF Edu Seg Tree : Step 2 + Step 3

---
+++ DS implementation/Segment tree/First element at least X.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../../../debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline long long gcd(long long a, long long b) {long long r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline long long lcm(long long a, long long b) {return a / gcd(a, b) * b;}
+
+template<typename T>
+struct SegmentTree {
+    int size;
+    vector<T> tree, arr;
+    int n;
+
+    T op(T a, T b) {
+        return max(a,b);
+    }
+
+    T e() {
+        return -(1e9);
+    }
+
+    SegmentTree(int n, vector<T> &arr) {
+
+        this->n = n;
+        this->arr = arr;       
+        
+        size = 4*n;
+        tree.assign(size,0);
+        
+        build(0,0,n-1);
+    }
+
+    void build(int v, int lb, int rb) {
+        if(lb == rb) {
+            tree[v] = arr[lb];
+        }
+        else {
+            int mid = (lb + rb) / 2;
+            
+            // 0-based indexing
+            int lv = 2*v+1; // left child index
+            int rv = 2*v+2; // right child index
+
+            build(lv, lb, mid);
+            build(rv, mid+1, rb);
+
+            tree[v] = op(tree[lv], tree[rv]);
+        }
+    }
+
+    void update(int v, int lb, int rb, int pos, T new_value) {
+        if(lb == rb) {
+            tree[v] = new_value; // update the value
+        }
+        else { // recomputation
+            int mid = (lb + rb) / 2;
+
+            // 0-based indexing
+            int lv = 2*v+1;
+            int rv = 2*v+2;
+
+            if(pos <= mid) { // element lies in the left segment
+                update(lv, lb, mid, pos, new_value);
+            }
+            else { // element lies in the right segment
+                update(rv, mid+1, rb, pos, new_value);
+            }
+
+            tree[v] = op(tree[lv],tree[rv]);
+        }
+    }
+
+    T query(int v, int lb, int rb, int l, int r) {
+
+        if(r < lb || rb < l) 
+            return e();
+  
+        if(lb >= l && rb <= r) {
+            return tree[v];
+        }
+
+        int mid = (lb + rb) / 2;
+
+        // 0-based indexing
+        int lv = 2*v+1;
+        int rv = 2*v+2;
+
+        return op(query(lv,lb,mid,l,r),query(rv,mid+1,rb,l,r));
+    }
+
+    // pos is 0-based indexing
+    void update(int pos, T new_value) {
+        update(0,0,n-1,pos,new_value);
+    }
+    // l included and r is excluded i.e. [l,r) 
+    T query(int l, int r) { 
+        return query(0,0,n-1,l,r-1);
+    }
+
+    int find(int x, int lx, int rx, T val) {
+        if(lx == rx) {
+            if(tree[x] >= val)
+                return lx;
+            return -1;
+        }
+
+        T lval = tree[2*x+1];
+        int mid = (lx + rx) / 2;
+        if(lval >= val) {
+            return find(2*x+1,lx,mid, val);
+        }
+        else 
+            return find(2*x+2,mid+1,rx,val);
+    }
+
+    int find(T val) {
+        return find(0,0,n-1,val);
+    }
+
+};
+
+void solve() {
+    long long n, m;
+    cin >> n >> m;
+
+    vector<long long> A(n);
+    for(auto &a: A) cin >> a;
+
+    SegmentTree<long long> st(n, A);
+
+    while(m--) {
+        int type;
+        cin >> type;
+
+        if(type == 1) { // update
+            int i;
+            long long val;
+            cin >> i >> val;
+            
+            st.update(i,val);
+        }
+        else if(type == 2) {
+            long long x;
+            cin >> x;
+            cout << st.find(x) << endl;
+        }
+
+
+    }
+}
+signed main() {
+    // your code goes here
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+        freopen("../../../error.txt", "w", stderr);
+    #endif
+    
+    long long t = 1;
+    // cin >> t;
+    
+    while(t--) {
+        solve();
+        
+        // if(t > 0) cout << endl;
+    }
+    cout<<fixed<<setprecision(10);
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
---
+++ DS implementation/Segment tree/Segment with the Maximum Sum_acl.cpp
@@ -0,0 +1,278 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../../../debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline long long gcd(long long a, long long b) {long long r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline long long lcm(long long a, long long b) {return a / gcd(a, b) * b;}
+
+struct node {
+    long long seg = 0, pref = 0, suff = 0, sum = 0;
+};
+
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
+#if __cplusplus >= 202002L
+#include <bit>
+#endif
+
+namespace atcoder {
+
+namespace internal {
+
+#if __cplusplus >= 202002L
+
+using std::bit_ceil;
+
+#else
+
+// @return same with std::bit::bit_ceil
+unsigned int bit_ceil(unsigned int n) {
+    unsigned int x = 1;
+    while (x < (unsigned int)(n)) x *= 2;
+    return x;
+}
+
+#endif
+
+// @param n `1 <= n`
+// @return same with std::bit::countr_zero
+int countr_zero(unsigned int n) {
+#ifdef _MSC_VER
+    unsigned long index;
+    _BitScanForward(&index, n);
+    return index;
+#else
+    return __builtin_ctz(n);
+#endif
+}
+
+// @param n `1 <= n`
+// @return same with std::bit::countr_zero
+constexpr int countr_zero_constexpr(unsigned int n) {
+    int x = 0;
+    while (!(n & (1 << x))) x++;
+    return x;
+}
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+namespace atcoder {
+
+#if __cplusplus >= 201703L
+
+template <class S, auto op, auto e> struct segtree {
+    static_assert(std::is_convertible_v<decltype(op), std::function<S(S, S)>>,
+                  "op must work as S(S, S)");
+    static_assert(std::is_convertible_v<decltype(e), std::function<S()>>,
+                  "e must work as S()");
+
+#else
+
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+
+#endif
+
+  public:
+    segtree() : segtree(0) {}
+    explicit segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    explicit segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        size = (int)internal::bit_ceil((unsigned int)(_n));
+        log = internal::countr_zero((unsigned int)size);
+        d = std::vector<S>(2 * size, e());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) const {
+        assert(0 <= p && p < _n);
+        return d[p + size];
+    }
+
+    S prod(int l, int r) const {
+        assert(0 <= l && l <= r && r <= _n);
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+
+    S all_prod() const { return d[1]; }
+
+    template <bool (*f)(S)> int max_right(int l) const {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*f)(S)> int min_left(int r) const {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+};
+
+}  // namespace atcoder
+
+
+
+node op(node a, node b) {
+    node c;
+    c.seg = max({a.seg, b.seg, a.suff + b.pref});
+    c.pref = max(a.pref, a.sum + b.pref);
+    c.suff = max(b.suff, b.sum + a.suff);
+    c.sum = a.sum + b.sum;
+    return c;
+}
+
+node e() {
+    return node{0LL,0LL,0LL,0LL};
+}
+
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+
+    vector<long long> A(n);
+    for(auto &a: A) cin >> a;
+
+    vector<node> v;
+    for(auto a: A) {
+        v.emplace_back(node{a,a,a,a});
+    }
+
+    atcoder::segtree<node, op, e> st(v);
+    cout << st.prod(0,n).seg << endl;
+    while(q--) {
+        int i; 
+        long long val;
+        cin >> i >> val;
+        st.set(i, node{val,val,val,val});
+        cout << st.prod(0,n).seg;
+        if(q) cout << endl;
+    }
+}
+signed main() {
+    // your code goes here
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+        freopen("../../../error.txt", "w", stderr);
+    #endif
+    
+    long long t = 1;
+    // cin >> t;
+    
+    while(t--) {
+        solve();
+        
+        if(t > 0) cout << endl;
+    }
+    cout<<fixed<<setprecision(10);
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
