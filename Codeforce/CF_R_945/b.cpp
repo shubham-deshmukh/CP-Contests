9dddc23065eb906f35821f7704bb643ad09be2d4
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2024-05-17 22:13:23 +0530

    Live CF R 945 (div2) : A,B

---
+++ Codeforce/CF_R_945/b.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../../../debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline long long gcd(long long a, long long b) {long long r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline long long lcm(long long a, long long b) {return a / gcd(a, b) * b;}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> A(n);
+    for(auto &a: A) cin >> a;
+
+    auto isOk = [&](int k)->bool {
+        vector<int> ans(20,0);
+        for(int i = 0; i < k; i++) {
+            for(int bit = 0; bit < 20; bit++) {
+                int mask = 1 << bit;
+                if(A[i]&mask) {
+                    ans[bit] += 1;
+                }
+            }
+        }
+        vector<int> temp = ans;
+        for(int i = 1; i+k-1 < n; i++) {
+            // i + k - 1
+            for(int bit = 0; bit < 20; bit++) {
+                int mask = 1 << bit;
+                if(A[i-1]&mask) {
+                    temp[bit] -= 1;
+                }
+            }
+            for(int bit = 0; bit < 20; bit++) {
+                int mask = 1 << bit;
+                if(A[i+k-1]&mask) {
+                    temp[bit] += 1;
+                }
+            }         
+
+            for(int bit = 0; bit < 20; bit++) {
+                if(temp[bit] and !ans[bit]) {
+                    return false;
+                }
+                if(!temp[bit] and ans[bit]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+
+    int l = 0, r = n;
+    while(r-l > 1) {
+        int mid = l + (r-l) / 2;
+        if(isOk(mid)) {
+            r = mid;
+        }
+        else {
+            l = mid;
+        }
+    }
+    cout << r;
+}
+signed main() {
+    // your code goes here
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+        freopen("../../../error.txt", "w", stderr);
+    #endif
+    
+    long long t = 1;
+    cin >> t;
+    
+    while(t--) {
+        solve();
+        
+        if(t > 0) cout << endl;
+    }
+    cout<<fixed<<setprecision(10);
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
