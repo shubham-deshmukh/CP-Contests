f32e1c358a0e4a2eb52ad83becb00ea1ab0ff191
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2024-12-14 19:19:51 +0530

    Live Atcoder ABC384: A,B,C,D,E

---
+++ Atcoder/abc384/c.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../../../debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline long long gcd(long long a, long long b) {long long r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline long long lcm(long long a, long long b) {return a / gcd(a, b) * b;}
+
+
+void solve() {
+
+    vector<int> score(5);
+    for(auto &a: score) cin >> a;
+
+    vector<string> A;
+    string s = "ABCDE";
+    string t;
+    auto rec = [&](const auto &self, int i)-> void{
+        if(i == 5) {
+            if(!t.empty())
+                A.push_back(t);
+            return;
+        }
+
+        self(self,i+1);
+        t.push_back(s[i]);
+        self(self,i+1);
+        t.pop_back();
+    };
+
+    rec(rec,0);
+    // debug(A);
+    auto getScore = [&](string s)->int {
+        int tot = 0;
+        for(auto ch: s)
+            tot += score[ch-'A'];
+        return tot;
+    };
+
+    sort(A.begin(), A.end(),[&](string a, string b) {
+        int score1 = getScore(a), score2 = getScore(b);
+        if(score1 == score2) {
+            return a < b;
+        }
+
+        return score1 > score2;
+    });
+
+    for(auto a: A) cout << a << endl;
+   
+}
+signed main() {
+    // your code goes here
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+        freopen("../../../error.txt", "w", stderr);
+    #endif
+    
+    solve();
+    
+    cout<<fixed<<setprecision(10);
+    cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
