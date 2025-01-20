9894f9ea96fda4cebc88603d1214a1742fc5d6a1
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2025-01-20 10:07:17 +0530

    Live Atcoder ABC389: A,B,C

---
+++ Atcoder/abc389/C_Snake_Queue.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// #ifndef ONLINE_JUDGE
+// #include "./debug.h"
+// #else
+// #define debug(x...)
+// #endif
+
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline long long gcd(long long a, long long b) {long long r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline long long lcm(long long a, long long b) {return a / gcd(a, b) * b;}
+
+#define int int64_t
+using pii = pair<int,int>;
+
+void solve() {
+    int Q;
+    cin >> Q;
+
+    deque<pii> q; // head, length
+    int curr = 0;
+    int prev = 0;
+    while(Q--) {
+        int type;
+        cin >> type;
+
+        if(type == 1) {
+            int len;
+            cin >> len;
+            q.push_back({prev,len});
+            prev += len;
+        }
+        else if(type == 2) {
+            curr += q.front().second;
+            q.pop_front();
+        }
+        else if(type == 3) {
+            int k;
+            cin >> k;
+            int head = q[k-1].first;
+            cout << head - curr << endl;
+        }
+    }
+}
+signed main() {
+    // your code goes here
+    fast;
+    // #ifndef ONLINE_JUDGE
+    //     freopen("./input.txt", "r", stdin);
+    //     freopen("./output.txt", "w", stdout);
+    //     freopen("./error.txt", "w", stderr);
+    // #endif
+    
+    solve();
+    
+    // cout<<fixed<<setprecision(10);
+    // cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
