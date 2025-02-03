bc06105f4321fc8ae544738dd7bb9d288b2bb9e6
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2025-02-03 15:47:44 +0530

    Live ABC 391: A,B,C

---
+++ Atcoder/abc391/B_Seek_Grid.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// #ifndef ONLINE_JUDGE
+// #include "./debug.h"
+// #else
+// #define debug(x...)
+// #endif
+
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline long long gcd(long long a, long long b) {long long r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline long long lcm(long long a, long long b) {return a / gcd(a, b) * b;}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<string> s(n);
+    vector<string> t(m);
+
+    for(auto &a: s) cin >> a;
+    for(auto &a: t) cin >> a;
+
+    for(int i = 0; i+m-1 < n; i++) {
+        for(int j = 0; j+m-1 < n; j++) {
+            bool isFound = true;
+            for(int ii = 0; ii < m; ii++) {
+                for(int jj = 0; jj < m; jj++) {
+                    isFound &= (t[ii][jj] == s[ii+i][jj+j]);
+                }
+            }
+            if(isFound) {
+                cout << i + 1 << " " << j + 1;
+                return;
+            }
+        }
+    }
+}
+signed main() {
+    // your code goes here
+    fast;
+    // #ifndef ONLINE_JUDGE
+    //     freopen("./input.txt", "r", stdin);
+    //     freopen("./output.txt", "w", stdout);
+    //     freopen("./error.txt", "w", stderr);
+    // #endif
+    // int t; cin >> t; while(t--)
+    solve();
+    
+    // cout<<fixed<<setprecision(10);
+    // cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
