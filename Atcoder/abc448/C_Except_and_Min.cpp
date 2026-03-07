214d6ce59d18636a570879e551f60e0b2ffe67a9
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2026-03-07 19:07:18 +0530

    Live ABC448: A,B,C,D

---
+++ Atcoder/abc448/C_Except_and_Min.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// #ifndef ONLINE_JUDGE
+// #include "./debug.h"
+// #else
+// #define debug(x...)
+// #endif
+
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace __gnu_pbds;
+
+template<class T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// X.find_by_order(k) return kth element. 0 indexed.
+// X.order_of_key(k) returns count of elements strictly less than k.
+
+
+const int MAX_N = 1e5;
+const int mod = 1e9+7;
+
+inline long long gcd(long long a, long long b) {long long r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline long long lcm(long long a, long long b) {return a / gcd(a, b) * b;}
+
+using pii = pair<int,int>;
+void solve() {
+    int n, Q;
+    cin >> n >> Q;
+
+    vector<int> A(n);
+    for(auto &a: A) cin >> a;
+
+    multiset<pii> st;
+    for(int i = 0; i < n; i++) {
+        st.insert(pii{A[i],i});
+    }
+
+    for(int q = 0; q < Q; q++) {
+        int k;
+        cin >> k;
+
+        vector<int> B(k);
+        for(auto &b: B) cin >> b;
+
+        // remove balls from the bag
+        for(auto &b: B) {
+            auto it = st.find(pii{A[b-1],b-1});
+            st.erase(it);
+        }
+        cout << st.begin()->first << "\n";
+        // insert the removed balls
+        for(auto &b: B) {
+            st.insert(pii{A[b-1],b-1});
+        }
+    }
+}
+signed main() {
+    // your code goes here
+    fast;
+    // #ifndef ONLINE_JUDGE
+    //     freopen("./input.txt", "r", stdin);
+    //     freopen("./output.txt", "w", stdout);
+    //     freopen("./error.txt", "w", stderr);
+    // #endif
+    // int t; cin >> t; while(t--)
+    solve();
+    
+    // cout<<fixed<<setprecision(10);
+    // cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";    
+    return 0;
+}
